b51e675554c3a39489ca8616dd0ee3bccbc7378b
Paul Darcey <pdarcey@gmail.com>
2017-08-05 14:42:22 +1000

    Updated replacement list
    Also moved regex into a forEach loop to make it more readable

--- Bullshit.safariextension/bullshit.js
+++ Bullshit.safariextension/bullshit.js
@@ -28,21 +28,32 @@ function walk(node) {
 
 function handleText(textNode)  {
 	var v = textNode.nodeValue;
+	var list = [ "says", "said", "saying", "claims", "claiming", "claimed", "tells", "telling", "told", "responds", "responding", "responded", "rejects", "rejecting", "rejected" ];
 	
-	v = v.replace(/says.*?["“‘]?.*?(\.\ )[?!;]?["”’]?/g, function myFunction(x){return x + " despite all evidence to the contrary. ";});
-	v = v.replace(/said.*?["“‘]?.*?(\.\ )[?!;]?["”’]?/g, function myFunction(x){return x + " despite all evidence to the contrary. ";});
-	v = v.replace(/saying.*?["“‘]?.*?(\.\ )[?!;]?["”’]?/g, function myFunction(x){return x + " despite all evidence to the contrary. ";});
-	v = v.replace(/claims.*?["“‘]?.*?(\.\ )[?!;]?["”’]?/g, function myFunction(x){return x + " despite all evidence to the contrary. ";});
-	v = v.replace(/claiming.*?["“‘]?.*?(\.\ )[?!;]?["”’]?/g, function myFunction(x){return x + " despite all evidence to the contrary. ";});
-	v = v.replace(/claimed.*?["“‘]?.*?(\.\ )[?!;]?["”’]?/g, function myFunction(x){return x + " despite all evidence to the contrary. ";});
-	v = v.replace(/tells.*?["“‘]?.*?(\.\ )[?!;]?["”’]?/g, function myFunction(x){return x + " despite all evidence to the contrary. ";});
-	v = v.replace(/telling.*?["“‘]?.*?(\.\ )[?!;]?["”’]?/g, function myFunction(x){return x + " despite all evidence to the contrary. ";});
-	v = v.replace(/told.*?["“‘]?.*?(\.\ )[?!;]?["”’]?/g, function myFunction(x){return x + " despite all evidence to the contrary. ";});
-	v = v.replace(/responds.*?["“‘]?.*?(\.\ )[?!;]?["”’]?/g, function myFunction(x){return x + " despite all evidence to the contrary. ";});
-	v = v.replace(/responding.*?["“‘]?.*?(\.\ )[?!;]?["”’]?/g, function myFunction(x){return x + " despite all evidence to the contrary. ";});
-	v = v.replace(/responded.*?["“‘]?.*?(\.\ )[?!;]?["”’]?/g, function myFunction(x){return x + " despite all evidence to the contrary. ";});
+	list.forEach(function(word) {
+	    var find = word + ".*?[\"“‘]?.*?\\."
+	    var regex = new RegExp(find,"g");
+	    v = v.replace(regex, function replacement(found) {
+	        console.log('Result is' + found);
+	        return found + " despite all evidence to the contrary.";
+	    });
+	});
 	
-    v = v.replace(/\.  despite all evidence to the contrary\./g,", despite all evidence to the contrary.");
+// 	v = v.replace(/says.*?["“‘]?.*?(\.[\ \n])[?!;]?["”’]?/g, function myFunction(x){return x + ", despite all evidence to the contrary.";});
+// 	v = v.replace(/said.*?["“‘]?.*?(\.[\ \n])[?!;]?["”’]?/g, function myFunction(x){return x + " despite all evidence to the contrary. ";});
+// 	v = v.replace(/saying.*?["“‘]?.*?(\.[\ \n])[?!;]?["”’]?/g, function myFunction(x){return x + " despite all evidence to the contrary. ";});
+// 	v = v.replace(/claims.*?["“‘]?.*?(\.[\ \n])[?!;]?["”’]?/g, function myFunction(x){return x + " despite all evidence to the contrary. ";});
+// 	v = v.replace(/claiming.*?["“‘]?.*?(\.[\ \n])[?!;]?["”’]?/g, function myFunction(x){return x + " despite all evidence to the contrary. ";});
+// 	v = v.replace(/claimed.*?["“‘]?.*?(\.[\ \n])[?!;]?["”’]?/g, function myFunction(x){return x + " despite all evidence to the contrary. ";});
+// 	v = v.replace(/tells.*?["“‘]?.*?(\.[\ \n])[?!;]?["”’]?/g, function myFunction(x){return x + " despite all evidence to the contrary. ";});
+// 	v = v.replace(/telling.*?["“‘]?.*?(\.[\ \n])[?!;]?["”’]?/g, function myFunction(x){return x + " despite all evidence to the contrary. ";});
+// 	v = v.replace(/told.*?["“‘]?.*?(\.[\ \n])[?!;]?["”’]?/g, function myFunction(x){return x + " despite all evidence to the contrary. ";});
+// 	v = v.replace(/responds.*?["“‘]?.*?(\.[\ \n])[?!;]?["”’]?/g, function myFunction(x){return x + " despite all evidence to the contrary. ";});
+// 	v = v.replace(/responding.*?["“‘]?.*?(\.[\ \n])[?!;]?["”’]?/g, function myFunction(x){return x + " despite all evidence to the contrary. ";});
+// 	v = v.replace(/responded.*?["“‘]?.*?(\.[\ \n])[?!;]?["”’]?/g, function myFunction(x){return x + " despite all evidence to the contrary. ";});
+	
+    v = v.replace(/\. despite all evidence to the contrary\./g,", despite all evidence to the contrary.");
+    v = v.replace(/\.\n  despite all evidence to the contrary\./g,", despite all evidence to the contrary.");
     v = v.replace(/\.\"  despite all evidence to the contrary\./g,",\" despite all evidence to the contrary.");
     v = v.replace(/\.”  despite all evidence to the contrary\./g,",” despite all evidence to the contrary.");
     v = v.replace(/despite all evidence to the contrary. despite all evidence to the contrary./g,"despite all evidence to the contrary.");
