504a48f2fbdeb20bb8009fe8a2f5fc8af4fb1319
Paul Darcey <pdarcey@gmail.com>
2017-08-15 17:35:40 +1000

    Refinements to the replace algorithm

--- Bullshit.safariextension/bullshit.js
+++ Bullshit.safariextension/bullshit.js
@@ -28,22 +28,32 @@ function walk(node) {
 
 function handleText(textNode)  {
 	var v = textNode.nodeValue;
+	// List of words to look for before quote
 	var list = [ "says", "said", "saying", "claims", "claiming", "claimed", "tells", "telling", "told", "responds", "responding", "responded", "rejects", "rejecting", "rejected" ];
 	
+	// Preliminary cleanup. Replaces 3 x . with ellipse, and abbreviations with dots with same without dots
+	v = v.replace(/U\.S\.A\./g,"USA");
+	v = v.replace(/U\.S\./g,"US");
+	v = v.replace(/U\.S,/g,"US,");
+    v = v.replace(/\.\.+\ ?/g,"… ");
+	
+	// Actual replacement
 	list.forEach(function(word) {
-	    var find = word + ".*?[\"“‘]?.*?[.!?]((?=\\s[A-z])|$)"
+	    var find = word + ".*?[.!?]”?((?=\\s[A-z])|$)" // Looks for word then quotes then end of sentence/paragraph
 	    var regex = new RegExp(find,"g");
 	    v = v.replace(regex, function replacement(found) {
-	        console.log('Result is' + found);
+	        // console.log('Result is' + found);
 	        return found + " despite all evidence to the contrary.";
 	    });
 	});
 	
+	// Post-replacement cleanup
     v = v.replace(/\. despite all evidence to the contrary\./g,", despite all evidence to the contrary.");
     v = v.replace(/\.\n  despite all evidence to the contrary\./g,", despite all evidence to the contrary.");
     v = v.replace(/\.\"  despite all evidence to the contrary\./g,",\" despite all evidence to the contrary.");
     v = v.replace(/\.”  despite all evidence to the contrary\./g,",” despite all evidence to the contrary.");
     v = v.replace(/despite all evidence to the contrary. despite all evidence to the contrary./g,"despite all evidence to the contrary.");
 	
+	// Save back to node
 	textNode.nodeValue = v;
 }
